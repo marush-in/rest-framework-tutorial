19ebce0214007745479f56c83bdaed9c9addbe4a
marush-in <shin.marumoto@gmail.com>
2019-01-21 14:04:15 +0900

    Tutorial 7: Schemas & client libraries

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 from snippets import views
 
 router = DefaultRouter()
 router.register('snippets', views.SnippetViewSet)
 router.register('users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
+
 urlpatterns = [
+    path('schema/', schema_view),
     path('', include(router.urls)),
 ]
